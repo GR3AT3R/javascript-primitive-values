4a542b3b5508cb2e0ab9f8cd242b45db5d2d8cd4
Gett <phonlachet.t@gmail.com>
2025-02-12 16:20:17 +0700

    Submit

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,10 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+const totalPrice = 27000;
+const discountAmount = totalPrice * 0.25;
+const finalPrice = totalPrice - discountAmount;
+
+console.log("Total Price:", totalPrice);          // Output: 27000
+console.log("Discount Amount:", discountAmount);  // Output: 6750
+console.log("Final Price:", finalPrice);          // Output: 20250
--- ex-7.js
+++ ex-7.js
@@ -4,4 +4,7 @@ let subject = "I";
 let verb = "am";
 let object = "A Software Developer";
 
-let sentence;
+let sentence1 = subject + " " + verb + " " + object;
+let sentence2 = `\"${subject} ${verb} ${object}\"`;
+console.log(sentence1);
+console.log(sentence2);
